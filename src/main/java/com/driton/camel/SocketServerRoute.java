31f023ec090eef0da2012ac28fe9e9d391ba09bf
Driton Salihu <driton.geci@gmail.com>
2024-01-25 10:17:54 -0500

    Add SocketServer Route.

---
+++ src/main/java/com/driton/camel/SocketServerRoute.java
@@ -0,0 +1,29 @@
+package com.driton.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+@Component
+@ConditionalOnProperty(name = "com.driton.camel.SocketServerRoute.enabled", havingValue = "true")
+public class SocketServerRoute extends RouteBuilder{
+    @Autowired
+    protected CamelContext context;
+
+    @Override
+    public void configure() throws Exception {
+                StringDecoder stringDecoder = new StringDecoder();
+        StringEncoder stringEncoder = new StringEncoder();
+        context.getRegistry().bind("stringEncoder", stringEncoder);
+        context.getRegistry().bind("stringDecoder", stringDecoder);
+
+        from("netty:tcp://localhost:3004?sync=true&allowDefaultCodec=false&encoders=#stringEncoder&decoders=#stringDecoder")
+        .log("Received on port 3004 ${body}");
+    }
+    
+}
