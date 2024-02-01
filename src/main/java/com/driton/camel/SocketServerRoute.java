b9920e36147e93f4c77dc086f34fcb514987889d
Driton Salihu <driton.geci@gmail.com>
2024-02-01 11:15:24 -0500

    Add two tcp routes on ports 3001 and 3002.

--- src/main/java/com/driton/camel/SocketServerRoute.java
+++ src/main/java/com/driton/camel/SocketServerRoute.java
@@ -11,18 +11,21 @@ import io.netty.handler.codec.string.StringEncoder;
 
 @Component
 @ConditionalOnProperty(name = "com.driton.camel.SocketServerRoute.enabled", havingValue = "true")
-public class SocketServerRoute extends RouteBuilder{
+public class SocketServerRoute extends RouteBuilder {
     @Autowired
     protected CamelContext context;
 
     @Override
     public void configure() throws Exception {
-                StringDecoder stringDecoder = new StringDecoder();
+        StringDecoder stringDecoder = new StringDecoder();
         StringEncoder stringEncoder = new StringEncoder();
         context.getRegistry().bind("stringEncoder", stringEncoder);
         context.getRegistry().bind("stringDecoder", stringDecoder);
 
-        from("netty:tcp://localhost:3004?sync=true&allowDefaultCodec=false&encoders=#stringEncoder&decoders=#stringDecoder")
-        .log("Received on port 3004 ${body}");
+        from("netty:tcp://localhost:3001?sync=true&allowDefaultCodec=false&encoders=#stringEncoder&decoders=#stringDecoder")
+                .log("Received on port 3001 ${body}");
+
+        from("netty:tcp://localhost:3002?sync=true&allowDefaultCodec=false&encoders=#stringEncoder&decoders=#stringDecoder")
+                .log("Received on port 3002 ${body}");
     }
 }
